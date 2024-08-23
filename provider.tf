d9d0665c14428a105326aceee33eee4d23c1cd02
Gutherry Ferreira <gutherry.ferreira@gmail.com>
2024-08-23 13:39:10 -0300

    tag

---
+++ provider.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.64.0"
+    }
+  }
+  backend "s3" {
+    bucket = "treinamento-terraform-compass"
+    key    = "dev/terraform.tfstate"
+    region = "us-west-2"
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-2"
+}
