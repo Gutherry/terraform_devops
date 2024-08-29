cf66dd77afe5b94b980fc48ed3a1337cd28ae321
Gutherry Ferreira <gutherry.ferreira@gmail.com>
2024-08-29 19:47:44 -0300

    feat : vp.tf

--- vpc.tf
+++ vpc.tf
@@ -1,9 +1,9 @@
-resource "aws_vpc" "main" {
-  cidr_block           = "10.0.0.0/16"
+resource "aws_vpc" "eks_vpc" {
+  cidr_block           = var.cidr_block
   enable_dns_support   = true
   enable_dns_hostnames = true
   tags = {
     Name = "gferreira-vpc"
-    Name = "teste"
+    Name = "VPC-DevOps"
   }
 }
