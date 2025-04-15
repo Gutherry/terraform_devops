cfa3b2443b9d3dc4344497b8cfce49125e8e8ab6
Gutherry Ferreira <gutherry.ferreira@gmail.com>
2025-04-15 16:24:21 -0300

    Feat: region.tf, variables.tf

--- public.tf
+++ public.tf
@@ -1,25 +1,29 @@
 resource "aws_subnet" "eks_subnet_public_1a" {
   vpc_id                  = aws_vpc.eks_vpc.id
-  cidr_block              = cidrsubnet(var.cidr_block, 8, 1)
-  availability_zone       = "us-east-1a"
+  cidr_block              = cidrsubnet(var.cidr_block, 8, 1) # 10.0.1.0/24
+  availability_zone       = "${data.aws_region.current.name}a"
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "Devops subenet 1a"
+    Name                        = "Devops subenet 1a",
+    "kubernetes.io/role/elb"    = 1
+    "kubernetes.io/cluster/eks" = "owned"
   }
 }
 
 resource "aws_subnet" "eks_subnet_public_1b" {
   vpc_id                  = aws_vpc.eks_vpc.id
   cidr_block              = cidrsubnet(var.cidr_block, 8, 2)
-  availability_zone       = "us-east-1b"
+  availability_zone       = "${data.aws_region.current.name}b"
   map_public_ip_on_launch = true
 
   tags = {
-    Name        = "Devops subenet 1b",
-    Owner       = "Gutherry Ferreira"
-    Environment = "Production"
-    Department  = "IT"
-    Project     = "Migration"
+    Name                        = "Devops subenet 1b",
+    "kubernetes.io/role/elb"    = 1
+    "kubernetes.io/cluster/eks" = "owned"
+    Owner                       = "Gutherry Ferreira"
+    Environment                 = "Production"
+    Department                  = "IT"
+    Project                     = "Migration"
   }
 }
--- variables.tf
+++ variables.tf
@@ -1,4 +1,6 @@
 variable "cidr_block" {
   type        = string
   description = "Networking CIDR block to be used for the VPC"
+  # default     = "10.0.0.0/16"  # Valor padrão (opcional)
+
 }
